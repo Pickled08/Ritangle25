08088b76a103c38bf9391de6f25f89808826467b
Alex Howard (Pickled08) <alexhoward21@icloud.com>
2025-10-27 21:38:53 +0000

    attempt q3s1

---
+++ Q3S1/Alex/q3.py
@@ -0,0 +1,27 @@
+import time
+
+list = []
+c=0
+
+start = time.perf_counter()
+#===========================================#
+for i in range(11111,66667):
+
+    if "7" in str(i) or "8" in str(i) or "9" in str(i) or "0" in str(i):
+        pass
+    else:
+        list.append(i)
+
+for num in list:
+    if num % 25 == 0:
+        c=c+1
+
+p = c/len(list)
+
+print(f"{p:.3f}")
+#===========================================#
+end = time.perf_counter()
+
+# Convert to milliseconds
+elapsed_ms = (end - start) * 1000
+print(f"Execution time: {elapsed_ms:.3f} ms")
